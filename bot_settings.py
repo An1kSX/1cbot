486b9f9d73901353e8733e1968f2ca51a1ac7c61
An1kSX <70918749+An1kSX@users.noreply.github.com>
2023-07-04 23:18:14 +0500

    Update bot_settings.py

--- bot_settings.py
+++ bot_settings.py
@@ -3,8 +3,8 @@ from pyrogram import Client
 
 
 
-bot_token = '6234224055:AAEvejmRtN7ktbrA3asvLHm7LLQQEdwsQ7g'
-api_id = 1472978
-api_hash = '54ad916d47750d25ac20ebfc80b4efe4'
+bot_token = ''
+api_id = 0
+api_hash = ''
 
 bot = Client('api1CBot', api_id, api_hash, bot_token = bot_token)
